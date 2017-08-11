0b073077b3660f35bd47708b63619204ee55c29e
MrSkinface <mlevitskiy#@gmail.com>
2017-08-11 11:10:09 +0300

    push

---
+++ src/main/java/org/mike/gitbranches/App.java
@@ -0,0 +1,13 @@
+package org.mike.gitbranches;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
