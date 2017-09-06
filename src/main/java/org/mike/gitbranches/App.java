162086d586f6a0a080397ad296b154b827792686
MrSkinface <mlevitskiy#@gmail.com>
2017-09-06 08:42:40 +0300

    changes

--- src/main/java/org/mike/gitbranches/App.java
+++ src/main/java/org/mike/gitbranches/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println("Branch A");
         System.out.println("Branch A");
         System.out.println("Branch A");
+        System.out.println("some changes");
     }
 }
