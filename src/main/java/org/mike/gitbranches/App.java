18639edd917a1fdc3e17b193b3209cad1f9ef0e8
MrSkinface <mlevitskiy#@gmail.com>
2017-09-07 12:08:16 +0300

    another commit

--- src/main/java/org/mike/gitbranches/App.java
+++ src/main/java/org/mike/gitbranches/App.java
@@ -9,11 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Branch A");
-        System.out.println("Branch A");
-        System.out.println("Branch A");
-        System.out.println("Branch Afff");
-        System.out.println("Branch Afff");
-        System.out.println("Branch Afff");
+        System.out.println("another commit");
     }
 }
