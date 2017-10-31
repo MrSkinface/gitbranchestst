337fa93d521512a48af86338e78ab57c7456891b
Levitsky Michael <levitskym@edi.su>
2017-10-31 13:04:21 +0200

    comm 1

--- src/main/java/org/mike/gitbranches/App.java
+++ src/main/java/org/mike/gitbranches/App.java
@@ -1,14 +1,21 @@
 package org.mike.gitbranches;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
-        System.out.println("another commit");
+        Files.write(Paths.get("/home/levitsky/Документы/08092017/luk/pdotpr.base.bin"),
+                Base64.getEncoder().encode(
+                        Files.readAllBytes(Paths.get("/home/levitsky/Документы/08092017/luk/pdotpr.bin"))
+                ));
     }
 }
